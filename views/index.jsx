1fc3582cc05048954c63c198d27c3bddb5c4c1ab
Brian Stovall <hoppenmaus@gmail.com>
2016-03-02 15:48:46 -0500

    proptypes exercise passed

--- views/index.jsx
+++ views/index.jsx
@@ -18,25 +18,28 @@ class TodoList extends React.Component {
             <div className="todoList">
                 <table style={{border: "2px solid black"}}>
                     <tbody>
-                    <Todo title="Shopping">Milk</Todo>
-                    <Todo title="Hair cut">13:00</Todo>
+                      <Todo title="Shopping">Milk</Todo>
+                      <Todo title="Hair cut">13:00</Todo>
+        	      <Todo title="Learn React">15:00</Todo>
                     </tbody>
                 </table>
             </div>
         );
     }
 }
-
 class Todo extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td style={{border: "1px solid black"}}>{this.props.title}</td>
-                <td style={{border: "1px solid black"}}>{this.props.children}</td>
-            </tr>
-        );
+        render() {
+            return (
+                <tr>
+                    <td style={{border: "1px solid black"}}>{this.props.title}</td>
+                    <td style={{border: "1px solid black"}}>{this.props.children}</td>
+                </tr>
+            );
+        }
     }
-}
+    Todo.propTypes = {
+        title: React.PropTypes.string.isRequired
+    };
 
 class TodoForm extends React.Component {
     render() {
